e7f219c21697083eefae1ba554101425a69f4f57
Pedro Varela <pvarela@ncl.com>
2019-12-30 18:37:20 -0500

    Added custom view

---
+++ customview/src/main/java/com/example/customview/FanDialer.kt
@@ -0,0 +1,63 @@
+package com.example.customview
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+private enum class FanSpeed(val label: Int) {
+    OFF(R.string.fan_off),
+    LOW(R.string.fan_low),
+    MEDIUM(R.string.fan_medium),
+    HIGH(R.string.fan_high)
+}
+
+private const val RADIUS_OFFSET_LABEL = 30
+
+private const val RADIUS_OFFSET_INDICATOR = -35
+
+class FanDialer @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : View(context, attrs, defStyle) {
+
+    private var radius = 0.0f // Radius of the circle.
+    private var fanSpeed = FanSpeed.OFF // The active selection.
+    private val pointPosition: PointF = PointF(
+        0.0f,
+        0.0f
+    ) // position variable which will be used to draw label and indicator circle position
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+        textAlign = Paint.Align.CENTER
+        textSize = 55.0f
+        typeface = Typeface.create("", Typeface.BOLD)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = (kotlin.math.min(w, h) / 2.0 * 0.8).toFloat()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+        canvas?.drawCircle(pointPosition.x, pointPosition.y, radius / 12, paint)
+        val labelRadius = radius + RADIUS_OFFSET_LABEL
+        for (i in FanSpeed.values()) {
+            pointPosition.computeXYForSpeed(i, labelRadius)
+            val label = resources.getString(i.label)
+            canvas?.drawText(label, pointPosition.x, pointPosition.y, paint)
+        }
+    }
+
+    private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
+        val startAngle = PI * (9 / 8.0)
+        val angle = startAngle + pos.ordinal * (PI / 4)
+        x = (radius * cos(angle)).toFloat() + width / 2
+        y = (radius * sin(angle)).toFloat() + height / 2
+    }
+
+}
